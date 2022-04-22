0a1b5e7a890bd9acd6f454ba8ef57075ce28c2a0
Heloiza Mendes <helocursos@gmail.com>
2022-04-22 00:40:35 -0300

    refactoring and responsiveness

--- src/helpers/Imc.jsx
+++ src/helpers/Imc.jsx
@@ -1,5 +1,4 @@
 
-
 export const levels = [
     { title: 'Magreza', color: '#96A3AB', icon: 'down', imc: [0, 18.5] },
     { title: 'Normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9] },
@@ -8,15 +7,13 @@ export const levels = [
 
 ];
 
-
 export const calculaImc = (height, weight) => {
     const imc = weight / (height * height);
     for (let i in levels) {
-
         if (imc >= levels[i].imc[0] && imc < levels[i].imc[1]) {
-            levels[i].yourImc = imc;
-
-            return levels[i]
+            let levelCopy = { ...levels[1] };
+            levelCopy.yourImc = parseFloat(imc.toFixed(2));
+            return levelCopy
         }
 
     }
